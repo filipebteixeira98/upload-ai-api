1133ba153f7aa7b33405fa04f448ca2df11a72aa
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-09-28 21:01:17 -0300

    feat: added route that allows users to upload media file which it must be an audio

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { fastify } from 'fastify'
 
 import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
+
+app.register(uploadVideoRoute)
 
 app
   .listen({
